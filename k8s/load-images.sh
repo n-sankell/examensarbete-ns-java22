b54761d409f037ba47cc735c986fe73645ff7f39
niklas.sankell <niklas.sankell@gmail.com>
2024-05-16 15:15:08 +0200

    DEV - k8s git-hub image registry

--- k8s/load-images.sh
+++ k8s/load-images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "minikube image load ex-user-service:k8s"
-minikube image load ex-user-service:1.0
+echo "minikube image load e-user-service:k8s"
+minikube image load e-user-service:1.0
 echo "DONE"
-echo "minikube image load ex-midi-manager:k8s"
-minikube image load ex-midi-manager:1.0
+echo "minikube image load e-midi-manager:k8s"
+minikube image load e-midi-manager:1.0
 echo "DONE"
-echo "minikube image load ex-react-app:k8s"
-minikube image load ex-react-app:1.0
+echo "minikube image load e-react-app:k8s"
+minikube image load e-react-app:1.0
 echo "ALL DONE"
