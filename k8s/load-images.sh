73943c8dc9f5b25c2c06428432499276056aa285
niklas.sankell <niklas.sankell@gmail.com>
2024-05-16 14:18:33 +0200

    DEV - k8s manifests

--- k8s/load-images.sh
+++ k8s/load-images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "minikube image load tf-mysql:k8s"
-minikube image load tf-mysql:v1.0
+echo "minikube image load ex-user-service:k8s"
+minikube image load ex-user-service:1.0
 echo "DONE"
-echo "minikube image load tf-react-app:k8s"
-minikube image load tf-react-app:v1.0
+echo "minikube image load ex-midi-manager:k8s"
+minikube image load ex-midi-manager:1.0
 echo "DONE"
-echo "minikube image load tf-spring-boot-app:k8s"
-minikube image load tf-spring-boot-app:v1.0
+echo "minikube image load ex-react-app:k8s"
+minikube image load ex-react-app:1.0
 echo "ALL DONE"
